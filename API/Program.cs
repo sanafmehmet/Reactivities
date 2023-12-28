8c4f54b511b1ffd651afefe966756413c1e1a3d3
sanafmehmet <sanafmehmet@gmail.com>
2023-12-28 11:13:45 +0600

    End of section 5

--- API/Program.cs
+++ API/Program.cs
@@ -1,25 +1,19 @@
+using API.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+builder.Services.AddApplicationServices(builder.Configuration);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<DataContext>(opt => {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
 
-builder.Services.AddCors(opt => {
-    opt.AddPolicy("CorsPolicy", policy =>
-    {
-        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-    });
-});
 
 var app = builder.Build();
 
---
+++ Application/Activities/Delete.cs
@@ -0,0 +1,41 @@
+using Application.Core;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Delete
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Id);
+
+                //if (activity == null) return null;
+
+                _context.Remove(activity);
+
+                await _context.SaveChangesAsync();
+
+                // var result = await _context.SaveChangesAsync() > 0;
+
+                // if (!result) return Result<Unit>.Failure("Failed to delete the activity");
+
+                // return Result<Unit>.Success(Unit.Value);
+            }
+        }
+    }
+}
